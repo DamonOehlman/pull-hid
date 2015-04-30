98fb3b84ea24d8b68b9a39dce7cfe26e04fe13f6
Damon Oehlman <damon.oehlman@gmail.com>
2015-04-30 12:04:05 +1000

    Playing around with logitech bcc950 camera

--- examples/bcc950-ptz.js
+++ examples/bcc950-ptz.js
@@ -10,7 +10,23 @@ if (! device) {
   return console.log('Could not locate BCC950 webcam');
 }
 
-pull(
-  device.read(),
-  pull.drain(console.log)
-);
+// pull(
+//   device.read(),
+//   pull.drain(console.log)
+// );
+
+// return;
+
+console.log(device.hid);
+
+function panLeft() {
+  device.hid.write([0x21, 0x01, 0x0E, 0x00, 0x01, 0x00, 0xFF, 0x01, 0x00, 0x01]); // [0x88, 0x01, 0x06, 0x01, 0x01, 0x01, 0x01, 0x33, 0xFF]);
+
+  console.log('wrote data, waiting for response');
+  device.hid.read(function(err, data) {
+    console.log('got data: ', data);
+    panLeft();
+  });
+}
+
+panLeft();
